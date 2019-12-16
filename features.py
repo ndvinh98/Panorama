afc1f70d438ead84ff445d82ebbba1228e134eeb
koyomin9xz <kaiznguyen98@gmail.com>
2019-12-16 10:52:21 +0700

    update

--- features.py
+++ features.py
@@ -1,56 +1,85 @@
 import cv2
 import numpy as np
 
-def findAndDescribeFeatures(image):
-	'''find and describe features of image, @Return keypoints and features of img'''
+def findAndDescribeFeatures(image,opt='SURF'):
+	'''find and describe features of @image,
+	if opt='SURF', SURF algorithm is used.
+	if opt='SIFT', SIFT algorithm is used.
+	if opt='ORB', ORB algorithm is used.
+	@Return keypoints and features of img'''
 	#Getting gray image
 	grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	#Find and describe the features.
-	sift = cv2.xfeatures2d.SURF_create()
-	#Low sift = cv2.xfeatures2d.SIFT_create()
-
-	#Find interest points.
-	keypoints = sift.detect(grayImage, None)
-
-	#Computing features.
-	keypoints, features = sift.compute(grayImage, keypoints)
-
+	if opt=='SURF':
+		md = cv2.xfeatures2d.SURF_create()
+	if opt=='ORB':
+		md = cv2.ORB_create()
+	if opt=='SIFT':
+		md = cv2.xfeatures2d.SIFT_create()
+	#Find interest points and Computing features.
+	keypoints, features = md.detectAndCompute(grayImage, None)
 	#Converting keypoints to numbers.
-	keypoints = np.float32([kp.pt for kp in keypoints])
-
+	#keypoints = np.float32(keypoints)
+	features = np.float32(features)
 	return keypoints, features
 
-def matchFeatures(featuresA, featuresB):
-	'''matching features beetween 2 features, @return matches'''
-	# Slow: featureMatcher = cv2.DescriptorMatcher_create("BruteForce")
-	featureMatcher = cv2.DescriptorMatcher_create("FlannBased")
+def matchFeatures(featuresA, featuresB,ratio=0.75,opt='FB'):
+	'''matching features beetween 2 @features.
+	 If opt='FB', FlannBased algorithm is used.
+	 If opt='BF', BruteForce algorithm is used.
+	 @ratio is the Lowe's ratio test.
+	 @return matches'''
+	if opt=='BF':
+		featureMatcher = cv2.DescriptorMatcher_create("BruteForce")
+	if opt=='FB':
+		#featureMatcher = cv2.DescriptorMatcher_create("FlannBased")
+		FLANN_INDEX_KDTREE = 0
+		index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+		search_params = dict(checks = 50)
+		featureMatcher = cv2.FlannBasedMatcher(index_params, search_params)
+		
 
 	#performs k-NN matching between the two feature vector sets using k=2 
 	#(indicating the top two matches for each feature vector are returned).
 	matches = featureMatcher.knnMatch(featuresA,featuresB, k=2)
-	return matches
 
-def generateHomography(src_img, dst_img, ratio=0.75, ransacRep=5.0):
-	'''@Return Homography matrix, @param src_img is the image which is transforming by homography,
+	# store all the good matches as per Lowe's ratio test.
+	good = []
+	for m,n in matches:
+		if m.distance<ratio*n.distance:
+			good.append(m)
+	if len(good)>4:
+		return good
+	else:
+		raise Exception("Not enought matches") 
+
+def generateHomography(src_img, dst_img, ransacRep=5.0):
+	'''@Return Homography matrix, @param src_img is the image which is warped by homography,
 	@param dst_img is the image which is choosing as pivot, @param ratio is the David Lowe’s ratio,
 	@param ransacRep is the maximum pixel “wiggle room” allowed by the RANSAC algorithm
 	'''
 
 	src_kp,src_features=findAndDescribeFeatures(src_img)
 	dst_kp,dst_features=findAndDescribeFeatures(dst_img)
-	matches=matchFeatures(src_features,dst_features)
-	good = []
-	for m,n in matches:
-		# Lowe's ratio test
-		if m.distance<ratio*n.distance:
-			good.append(m)
 
-	src_points = np.float32([src_kp[m.queryIdx] for m in good])
-	dst_points = np.float32([dst_kp[m.trainIdx] for m in good])
+	good=matchFeatures(src_features,dst_features)
+	
+	src_points = np.float32([src_kp[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+	dst_points = np.float32([dst_kp[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
-	if len(good)>4:
-		H, status = cv2.findHomography(src_points, dst_points, cv2.RANSAC,ransacRep)
-		return H
-	else:
-		return None
+	H, mask  = cv2.findHomography(src_points, dst_points, cv2.RANSAC,ransacRep)
+	matchesMask = mask.ravel().tolist()
+	return H,matchesMask
+	
+
+def drawKeypoints(img,kp):
+	img1=img
+	cv2.drawKeypoints(img,kp,img1,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	return img1
+
+def drawMatches(src_img,src_kp,dst_img,dst_kp,matches,matchesMask):
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask[:100], # draw only inliers
+                   flags = 2)
+    return cv2.drawMatches(src_img,src_kp,dst_img,dst_kp,matches[:100],None,**draw_params)
--- stitch.py
+++ stitch.py
@@ -4,7 +4,7 @@ import features
 import utils
 
 def blendingMask(height, width, barrier, smoothing_window, left_biased=True):
-
+    assert barrier < width
     mask = np.zeros((height, width))
     
     offset = int(smoothing_window/2)
@@ -25,52 +25,60 @@ def blendingMask(height, width, barrier, smoothing_window, left_biased=True):
     
     return cv2.merge([mask, mask, mask])
     
-def panoramaBlending(dst_img,src_img,width_original_dst,side,smoothing_window = 400,showstep=False):
-    h,w,_=dst_img.shape
-    barrier = width_original_dst -int(smoothing_window/2)
+def panoramaBlending(dst_img_rz,src_img_warped,width_dst,side,showstep=False):
+    '''Given two aligned images @dst_img and @src_img_warped, and the @width_dst is width of dst_img 
+    before resize, that indicates where there is the discontinuity between the images, 
+    this function produce a smoothed transient in the overlapping.
+    @smoothing_window is a parameter that determines the width of the transient
+    left_biased is a flag that determines whether it is masked the left image,
+    or the right one'''
+
+    h,w,_=dst_img_rz.shape
+    smoothing_window=int(width_dst/8)
+    barrier = width_dst -int(smoothing_window/2)
     mask1 = blendingMask(h, w, barrier, smoothing_window = smoothing_window, left_biased = True)
     mask2 = blendingMask(h, w, barrier, smoothing_window = smoothing_window, left_biased = False)
 
     if showstep:
-        nonblend=src_img+dst_img
+        nonblend=src_img_warped+dst_img_rz
     else:
         nonblend=None
         leftside=None
         rightside=None
 
     if side=='left':
-        dst_img=cv2.flip(dst_img,1)
-        src_img=cv2.flip(src_img,1)
-        dst_img=(dst_img*mask1)
-        src_img=(src_img*mask2)
-        pano=src_img+dst_img
+        dst_img_rz=cv2.flip(dst_img_rz,1)
+        src_img_warped=cv2.flip(src_img_warped,1)
+        dst_img_rz=(dst_img_rz*mask1)
+        src_img_warped=(src_img_warped*mask2)
+        pano=src_img_warped+dst_img_rz
         pano=cv2.flip(pano,1)
         if showstep:
-            leftside=cv2.flip(src_img,1)
-            rightside=cv2.flip(dst_img,1)
+            leftside=cv2.flip(src_img_warped,1)
+            rightside=cv2.flip(dst_img_rz,1)
     else:
-        dst_img=(dst_img*mask1)
-        src_img=(src_img*mask2)
-        pano=src_img+dst_img
+        dst_img_rz=(dst_img_rz*mask1)
+        src_img_warped=(src_img_warped*mask2)
+        pano=src_img_warped+dst_img_rz
         if showstep:
-            leftside=dst_img
-            rightside=src_img
+            leftside=dst_img_rz
+            rightside=src_img_warped
 
     
     return pano,nonblend,leftside,rightside
 
-def warpTwoImages(src_img, dst_img,smoothing_window = 400,showstep=False):
+def warpTwoImages(src_img, dst_img,showstep=False):
 
 	#generate Homography matrix
-    H=features.generateHomography(src_img,dst_img)
+    H,_=features.generateHomography(src_img,dst_img)
 
 	#get height and width of two images
-    h1,w1 = src_img.shape[:2]
-    h2,w2 = dst_img.shape[:2]
+    height_src,width_src = src_img.shape[:2]
+    height_dst,width_dst = dst_img.shape[:2]
 
-	#extract conners of two images
-    pts1 = np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
-    pts2 = np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
+	#extract conners of two images: top-left, bottom-left, bottom-right, top-right
+    pts1 = np.float32([[0,0],[0,height_src],[width_src,height_src],[width_src,0]]).reshape(-1,1,2)
+    pts2 = np.float32([[0,0],[0,height_dst],[width_dst,height_dst],[width_dst,0]]).reshape(-1,1,2)
 
 	#aply homography to conners of src_img
     pts1_ = cv2.perspectiveTransform(pts1, H)
@@ -84,35 +92,34 @@ def warpTwoImages(src_img, dst_img,smoothing_window = 400,showstep=False):
  
     #top left point of image which apply homography matrix, which has x coordinate < 0, has side=left
     #otherwise side=right
-    #transformed image is merged to the left side or right side of pivot image
+    #source image is merged to the left side or right side of destination image
     if(pts[0][0][0]<0):
         side='left'
-        width_pano=w2+t[0]
+        width_pano=width_dst+t[0]
     else:
         width_pano=int(pts1_[3][0][0])
         side='right'
     height_pano=ymax-ymin
 
-    #generating height and width of panorama
-    # translate
+    #Translation 
+    #https://stackoverflow.com/a/20355545
     Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) 
-    src_img_wrapped = cv2.warpPerspective(src_img, Ht.dot(H), (width_pano,height_pano))
-
-    #generating size of src_img_wrapped which has the same size as dst_img
+    src_img_warped = cv2.warpPerspective(src_img, Ht.dot(H), (width_pano,height_pano))
+    #generating size of dst_img_rz which has the same size as src_img_warped
     dst_img_rz=np.zeros((height_pano,width_pano,3))
     if side=='left':
-        dst_img_rz[t[1]:h1+t[1]-abs(h2-h1),t[0]:w2+t[0]] = dst_img
+        dst_img_rz[t[1]:height_src+t[1],t[0]:width_dst+t[0]] = dst_img
     else:
-        dst_img_rz[t[1]:h1+t[1]-abs(h2-h1),:w2] = dst_img
+        dst_img_rz[t[1]:height_src+t[1],:width_dst] = dst_img
 
     #blending panorama
-    pano2,nonblend,leftside,rightside=panoramaBlending(dst_img_rz,src_img_wrapped,w2,side,smoothing_window=smoothing_window,showstep=showstep)
+    pano,nonblend,leftside,rightside=panoramaBlending(dst_img_rz,src_img_warped,width_dst,side,showstep=showstep)
 
-    #crop black region
-    pano2=crop(pano2,h2,pts)
-    return pano2,nonblend,leftside,rightside
+    #croping black region
+    pano=crop(pano,height_dst,pts)
+    return pano,nonblend,leftside,rightside
 
-def multiStitching(list_images,smoothing_window):
+def multiStitching(list_images):
     '''assume that the list_images was supplied in left-to-right order, choose middle image then 
     divide the array into 2 sub-arrays, left-array and right-array. Stiching middle image with each
     image in 2 sub-arrays. @param list_images is The list which containing images, @param smoothing_window is 
@@ -125,18 +132,18 @@ def multiStitching(list_images,smoothing_window):
     while len(left)>1:
         dst_img=left.pop()
         src_img=left.pop()
-        left_pano,_,_,_=warpTwoImages(src_img,dst_img,smoothing_window)
+        left_pano,_,_,_=warpTwoImages(src_img,dst_img)
         left_pano=left_pano.astype('uint8')
         left.append(left_pano)
 
     while len(right)>1:
         dst_img=right.pop()
         src_img=right.pop()
-        right_pano,_,_,_=warpTwoImages(src_img,dst_img,smoothing_window)
+        right_pano,_,_,_=warpTwoImages(src_img,dst_img)
         right_pano=right_pano.astype('uint8')
         right.append(right_pano)
 
-    #if width_right_pano > width_left_pano, Select right_pano as pivot. Otherwise is left_pano
+    #if width_right_pano > width_left_pano, Select right_pano as destination. Otherwise is left_pano
     if(right_pano.shape[1]>=left_pano.shape[1]):
         fullpano,_,_,_=warpTwoImages(left_pano,right_pano)
     else:
@@ -144,20 +151,20 @@ def multiStitching(list_images,smoothing_window):
     return fullpano
 
 def crop(panorama,h_dst,conners):
-    '''crop panorama based on image which choosing as pivot.
+    '''crop panorama based on destination.
     @param panorama is the panorama
-    @param h_dst is the hight of pivot image
-    @param conner is the tuple which containing 4 conners of transformed image and 
-    4 conners of pivot image'''
+    @param h_dst is the hight of destination image
+    @param conner is the tuple which containing 4 conners of warped image and 
+    4 conners of destination image'''
     #find max min of x,y coordinate
     [xmin, ymin] = np.int32(conners.min(axis=0).ravel() - 0.5)
     [xmax, ymax] = np.int32(conners.max(axis=0).ravel() + 0.5)
     t = [-xmin,-ymin]
     conners=conners.astype(int)
 
-    #conners[0][0][0] is the X coordinate of top-left point of transformed image
-    #If it has value<0, transformed image is merged to the left side of pivot image
-    #otherwise is merged to the right side of pivot image
+    #conners[0][0][0] is the X coordinate of top-left point of warped image
+    #If it has value<0, warp image is merged to the left side of destination image
+    #otherwise is merged to the right side of destination image
     if conners[0][0][0]<0:
         n=abs(-conners[1][0][0]+conners[0][0][0])
         panorama=panorama[t[1]:h_dst+t[1],n:,:]
@@ -167,3 +174,6 @@ def crop(panorama,h_dst,conners):
         else:
             panorama=panorama[t[1]:h_dst+t[1],0:conners[3][0][0],:]
     return panorama
+
+
+
